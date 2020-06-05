333bafbc1cbe8fbca5f509171ba8315edcaee633
Tyrel Clayton <tyrelclayton92@gmail.com>
2020-06-05 14:59:25 -0400

    add apollo client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-const App = () => <div>Hello World!</div>;
+const TEST_QUERY = gql`
+  {
+    users {
+      id
+      email
+      username
+    }
+  }
+`;
+
+const App = () => {
+  const { data } = useQuery(TEST_QUERY);
+
+  return (
+    <div>
+      <p>Hello World!</p>
+      {data && <p>{JSON.stringify(data)}</p>}
+    </div>
+  );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { ApolloProvider } from '@apollo/react-hooks';
+import apolloClient from './apolloClient';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
