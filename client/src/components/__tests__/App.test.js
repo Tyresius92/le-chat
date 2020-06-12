4a5fe7193e74721a245bc9ec55740bb446063718
marcgaj <marcgaj@gmail.com>
2020-06-12 17:57:56 -0400

    add chakra and a test util

--- client/src/components/__tests__/App.test.js
+++ client/src/components/__tests__/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from '../App';
 import { useQuery } from '@apollo/react-hooks';
 import Logger from '../../logger';
@@ -9,7 +8,7 @@ jest.mock('@apollo/react-hooks', () => ({ useQuery: jest.fn(() => ({})) }));
 
 describe('App', () => {
   it('renders hello world', () => {
-    const { getByText } = render(<App />);
+    const { getByText } = renderThemed(<App />);
     const componentText = getByText('Hello World!');
     expect(componentText).toBeInTheDocument();
   });
@@ -17,7 +16,7 @@ describe('App', () => {
   it('logs an error once if there is a query error', () => {
     useQuery.mockReturnValue({ error: 'testsplosion' });
     expect(Logger.error).toHaveBeenCalledTimes(0);
-    const { rerender } = render(<App />);
+    const { rerender } = renderThemed(<App />);
     expect(Logger.error).toHaveBeenCalledTimes(1);
     rerender();
     expect(Logger.error).toHaveBeenCalledTimes(1);
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { ThemeProvider } from "@chakra-ui/core";
+import { render } from "@testing-library/react"
+
+global.renderThemed = children => render(<ThemeProvider>{children}</ThemeProvider>)
